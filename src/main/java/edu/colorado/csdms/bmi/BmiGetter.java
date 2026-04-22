212ebdf9363df3a084a4372f905ccd5b32bea774
Michael Topsom <119038284+michael-topsom-deltares@users.noreply.github.com>
2026-04-22 13:10:40 +0200

    Get and set float values without default methods, expecting BMI to be 2.1

--- src/main/java/edu/colorado/csdms/bmi/BmiGetter.java
+++ src/main/java/edu/colorado/csdms/bmi/BmiGetter.java
@@ -9,18 +9,14 @@ package edu.colorado.csdms.bmi;
 public interface BmiGetter {
 
   void getValue(String varName, double[] dest);
-  default void getValue(String varName, float[] dest) {
-    throw new UnsupportedOperationException("Not supported yet.");
-  }
+  void getValue(String varName, float[] dest);
   void getValue(String varName, int[] dest);
   void getValue(String varName, String[] dest);
 
   <T> T getValuePtr(String varName);
 
   void getValueAtIndices(String varName, double[] dest, int[] indices);
-  default void getValueAtIndices(String varName, float[] dest, int[] indices) {
-    throw new UnsupportedOperationException("Not supported yet.");
-  }
+  void getValueAtIndices(String varName, float[] dest, int[] indices);
   void getValueAtIndices(String varName, int[] dest, int[] indices);
   void getValueAtIndices(String varName, String[] dest, int[] indices);
 }
--- src/main/java/edu/colorado/csdms/bmi/BmiSetter.java
+++ src/main/java/edu/colorado/csdms/bmi/BmiSetter.java
@@ -9,16 +9,12 @@ package edu.colorado.csdms.bmi;
 public interface BmiSetter {
 
   void setValue(String varName, double[] src);
-  default void setValue(String varName, float[] src) {
-    throw new UnsupportedOperationException("Not supported yet.");
-  }
+  void setValue(String varName, float[] src);
   void setValue(String varName, int[] src);
   void setValue(String varName, String[] src);
 
   void setValueAtIndices(String varName, int[] indices, double[] src);
-  default void setValueAtIndices(String varName, int[] indices, float[] src) {
-    throw new UnsupportedOperationException("Not supported yet.");
-  }
+  void setValueAtIndices(String varName, int[] indices, float[] src);
   void setValueAtIndices(String varName, int[] indices, int[] src);
   void setValueAtIndices(String varName, int[] indices, String[] src);
 }
